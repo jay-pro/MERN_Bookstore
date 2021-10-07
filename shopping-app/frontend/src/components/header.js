a2b44bd01e3b55403a8ce943e5e16020ac66d0bc
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-10-08 00:33:39 +0700

    fas icon from fontawesome.com

--- shopping-app/frontend/src/components/header.js
+++ shopping-app/frontend/src/components/header.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function header() {
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#home">Kuroneko Bookstore</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">
+                <i className="fas fa-shopping-cart"></i>
+                &nbsp; Cart
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <i className="fas fa-users"></i>
+                &nbsp; Sign in
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   );
